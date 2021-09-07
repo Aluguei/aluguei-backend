7b06d9cf0e3b82ed317f55fc4055d2e3d6ac8229
Samuel Ferracini <samferracini@gmail.com>
2021-09-07 13:02:32 -0300

    style: change products folder name to product

--- src/modules/product/products.module.ts
+++ src/modules/product/products.module.ts
@@ -1,7 +1,8 @@
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { Module } from '@nestjs/common'
+
 import { ProductsService } from './products.service'
 import { Product } from './product.entity'
-import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product])],
