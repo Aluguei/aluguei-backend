224d626f212bc49a3bd41517ddf8f93f6c64baa8
Samuel Ferracini <samferracini@gmail.com>
2021-09-08 19:54:15 -0300

    feat: add uuid validation to ResetPasswordPayload

--- src/modules/auth/resetPassword.payload.ts
+++ src/modules/auth/resetPassword.payload.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsNotEmpty, MinLength } from 'class-validator'
+import { IsNotEmpty, MinLength, IsUUID } from 'class-validator'
 import { SameAs } from '../common/validator/same-as.validator'
 
 export class ResetPasswordPayload {
@@ -7,6 +7,7 @@ export class ResetPasswordPayload {
     required: true
   })
   @IsNotEmpty({ message: 'token é obrigatório' })
+  @IsUUID(4, { message: 'token precisa ser um uuid' })
   token: string
 
   @ApiProperty({
