b43a1e0bcec8724eee60c5923a10c0d2d40540e1
Samuel Ferracini <samferracini@gmail.com>
2021-09-15 21:32:31 -0300

    feat: bypass jwt when requester is a mobile device

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -6,23 +6,33 @@ import { UnauthorizedError } from '@modules/common/utils'
 import { UsersService } from '@modules/users'
 import { authConfig } from '@config'
 
+import { Request } from 'express'
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly usersService: UsersService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: authConfig.jwt.secret
+      secretOrKey: authConfig.jwt.secret,
+      passReqToCallback: true
     })
   }
 
-  async validate({ iat, exp, id }: any, done) {
-    const timeDiff = exp - iat
-    if (timeDiff <= 0) throw new UnauthorizedError()
+  async validate(request: Request, { iat, exp, id }: any, done) {
+    const { headers } = request
 
     const user = await this.usersService.get(id)
     if (!user) throw new UnauthorizedError()
 
+    const { device = 'web' } = headers
+
+    if (device !== 'mobile') {
+      const timeDiff = exp - iat
+      if (timeDiff <= 0) throw new UnauthorizedError()
+    }
+
     delete user.password
+
     done(null, user)
   }
 }
