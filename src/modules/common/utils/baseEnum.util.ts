b994d02e4ee66caafaf9e71be9151070765b9006
Samuel Ferracini <samferracini@gmail.com>
2021-11-03 19:52:21 -0300

    feat: create base enum

--- src/modules/common/utils/baseEnum.util.ts
+++ src/modules/common/utils/baseEnum.util.ts
@@ -1,1 +1,16 @@
-export class BaseEnum {}
+export interface IBaseEnumValue {
+  sys: string
+  human: string
+}
+
+export class BaseEnum {
+  static values: IBaseEnumValue[]
+
+  static getHumanValues() {
+    return this.values.map((e) => e.human)
+  }
+
+  static getSysValues() {
+    return this.values.map((e) => e.sys)
+  }
+}
